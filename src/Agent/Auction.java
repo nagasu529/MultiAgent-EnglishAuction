89deb7d158de3014b37559f282c773fb6ab1144b
chiewchk <kitti.chiewchan@gmail.com>
2018-10-08 10:09:04 +1300

    Bidding information Added. English auction complete.

--- src/Agent/Auction.java
+++ src/Agent/Auction.java
@@ -2,38 +2,9 @@ package Agent;
 
 public class Auction {
 
-    double pricePerMM;
-    double maxSellingPrice;
-    double minSelleingPrice;
-    double increseRateAcution;
-
-
-    public double englishAuction(double bidderPrice, double acutioneerPrice, double chageRatePct, double pricePerMM){
-        changedPriceRate(chageRatePct, pricePerMM);
-        if (acutioneerPrice < bidderPrice){
-            double tempPrice = bidderPrice * increseRateAcution;
-            return tempPrice;
-
-        }else {
-            return acutioneerPrice;
-        }
-    }
-
-    public double dutchAcution(double bidderPrice, double auctioneerPrice, double chageRatePct, double pricePerMM){
-        changedPriceRate(chageRatePct, pricePerMM);
-        if (bidderPrice < auctioneerPrice){
-            double temPrice = auctioneerPrice * increseRateAcution;
-            return temPrice;
-        }else {
-             return auctioneerPrice;
-        }
-
-    }
-
     public double changedPriceRate(double changePerct, double pricePerMM){
         double temRriceRate = (changePerct/100)*pricePerMM;
         return temRriceRate;
-
     }
 
 }
---
+++ src/Agent/BiddingUI.java
@@ -0,0 +1,8 @@
+package Agent;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BiddingUI extends JFrame {
+
+}
